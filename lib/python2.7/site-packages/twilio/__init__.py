51f4f264b53e3633f6e67fae702cfb0cde15c041
Derek Kastner <dkastner@gmail.com>
2017-08-30 11:36:34 -0500

    Main app

--- lib/python2.7/site-packages/twilio/__init__.py
+++
@@ -1,3 +0,0 @@
-
-__version_info__ = ('6', '5', '2')
-__version__ = '.'.join(__version_info__)
--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import os
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
 from slackclient import SlackClient
+
+app = Flask(__name__)
+
 slack_client = SlackClient(os.environ.get('SLACK_TOKEN', None))
-slack_client.api_call("api.test")
+twilio_clint = Client()
+
+@app.route('/twilio', methods=['POST'])
+def twilio_post():
+    response = FessagingResponse()
+    request.form['From']
+    message = request.form['Body']
+    slack_client.api_call("chat.postMessage", channel="#harvey-shelters",
+                          text=message, username="shelterbot",
+                          icon_emoji=':house:')
+
+    return Response(response.toxml(), mimetype="text/xml"), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
