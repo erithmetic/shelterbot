1e24b8192a4007521af401ed848f1f1773da9dbc
Derek Kastner <dkastner@gmail.com>
2017-08-30 12:02:24 -0500

    Use slack webhook

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import os
 from flask import Flask, request, Response
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
-from slackclient import SlackClient
+import slackweb
 
 app = Flask(__name__)
 
-slack_client = SlackClient(os.environ.get('SLACK_TOKEN', None))
+slack = slackweb.Slack(url=os.environ.get('SLACK_WEBHOOK', None))
 twilio_clint = Client()
 
 @app.route('/twilio', methods=['POST'])
 def twilio_post():
     request.form['From']
     message = request.form['Body']
-    slack_client.api_call("chat.postMessage", channel="#harvey-shelters",
-                          text=message, username="shelterbot",
-                          icon_emoji=':house:')
+    slack.notify(channel="#harvey-shelters",
+                 text=message, username="shelterbot",
+                 icon_emoji=':house:')
 
     response = MessagingResponse()
     return str(response)
